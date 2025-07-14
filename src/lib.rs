70ea62a53cf398be823942eee1902dcbe3e1dde6
Austin Schey <aschey13@gmail.com>
2025-07-13 19:46:53 -0700

    fix build

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod ansi_256_to_16;
+#[cfg(feature = "convert")]
 mod color;
 #[cfg(feature = "convert")]
 mod convert;
 mod detect;
 
+#[cfg(feature = "convert")]
 pub use color::*;
 pub use detect::*;
 
